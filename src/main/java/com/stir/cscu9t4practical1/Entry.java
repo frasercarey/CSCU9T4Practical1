17a559c39142baddbeca8bbec3e86b6a3658aa6e
Fraser Carey <frc00044@students.stir.ac.uk>
2023-04-06 21:46:31 +0100

    Task 6 - Entry replaced/Additional classes inserted

--- src/main/java/com/stir/cscu9t4practical1/Entry.java
+++ src/main/java/com/stir/cscu9t4practical1/Entry.java
@@ -1,58 +1,60 @@
-// This class holds information about a single training session
 package com.stir.cscu9t4practical1;
 
 import java.util.Calendar;
+
+/**
+ *
+ * @author Fraser Carey
+ */
 public class Entry {
-  private String name;
-  private Calendar dateAndTime;
-  private float distance;
-  
-  public Entry (String n, int d, int m, int y, int h, int min, int s, float dist) {
-    name = n;
-    Calendar inst = Calendar.getInstance();
-    inst.set(y,m-1,d,h,min,s);
-    dateAndTime = inst;
-    distance = dist;
-  } //constructor
-  
-  public String getName () {
-    return name;
-  } //getName
-  
-  public int getDay () {
-    return dateAndTime.get(Calendar.DATE);
-  } //getDay
-  
-  public int getMonth () {
-    int month =  dateAndTime.get(Calendar.MONTH) + 1;
-    return month;
-  } //getMonth
-  
-  public int getYear () {
-    return dateAndTime.get(Calendar.YEAR);
-  } //getYear
+    
+    protected String name;
+    private Calendar dateAndTime;
+    protected float distance;
+    
+    public Entry(String n, int d, int m, int y, int h, int min, int s, float dist) {
+        name = n;
+        Calendar inst = Calendar.getInstance();
+        inst.set(y, m-1, d, h, min, s);
+        dateAndTime = inst;
+        distance = dist;
+    }
 
-  public int getHour () {
-    return dateAndTime.get(Calendar.HOUR);
-  } //getHour
+    public String getName() {
+        return name;
+    }
 
-  public int getMin () {
-    return dateAndTime.get(Calendar.MINUTE);
-  } //getMin
-
-  public int getSec () {
-    return dateAndTime.get(Calendar.SECOND);
-  } //getSec
-
-  public float getDistance () {
-    return distance;
-  } //getYear
-
-  public String getEntry () {
-   String result = getName()+" ran " + getDistance() + " km in "
-             +getHour()+":"+getMin()+":"+ getSec() + " on "
-             +getDay()+"/"+getMonth()+"/"+getYear()+"\n";
-   return result;
-  } //getEntry
+    public int getDay() {
+        return dateAndTime.get(Calendar.DATE);
+    }
+    
+    public int getMonth() {
+        return dateAndTime.get(Calendar.MONTH) + 1;
+    } 
    
-} // Entry
+    public int getYear() {
+        return dateAndTime.get(Calendar.YEAR);
+    }
+
+    public int getHour() {
+        return dateAndTime.get(Calendar.HOUR);
+    }
+
+    public int getMin() {
+        return dateAndTime.get(Calendar.MINUTE);
+    }
+
+    public int getSec() {
+        return dateAndTime.get(Calendar.SECOND);
+    }
+
+    public float getDistance() {
+        return distance;
+    }
+
+    public String getEntry() {
+        return getName()+" completed a distance of " + getDistance() + " km in "
+             + getHour() + ":" + getMin() + ":" + getSec() + " on "
+             + getDay()+ "/" +getMonth() + "/" + getYear() + "\n";
+    }
+}
---
+++ src/main/java/com/stir/cscu9t4practical1/Swim.java
@@ -0,0 +1,29 @@
+package com.stir.cscu9t4practical1;
+
+/**
+ *
+ * @author Fraser Carey
+ */
+public class Swim extends Entry {
+    
+    private String where;
+    
+    public Swim(String n, int d, int m, int y, int h, int min, int s, float dist, String where) {
+        super(n, d, m, y, h, min, s, dist);
+        this.where = where;
+    }
+    
+    public String getWhere() {
+        if (where.equalsIgnoreCase("pool"))
+            return "in a pool";
+        else
+            return "outdoors";
+    }
+    
+    @Override
+    public String getEntry() {
+        return getName() + " swam " + getDistance() + " km " + getWhere() + " in "
+             + getHour() + ":" + getMin() + ":" + getSec() + " on " + getDay() + "/" 
+             + getMonth() + "/" + getYear() + "\n";
+    }
+}
